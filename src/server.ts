72597b410e1769f76bfc518a75a80f74055e3dd5
Arshad Ullah <mdarshadullah835@gmail.com>
2023-09-04 18:00:12 +0600

    eslint ,prettier,husky,lint-stage setup done

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 async function main() {
   try {
-    await mongoose.connect(config.database_uri as string);
-    console.log("🎆😍 database connection successfully");
+    await mongoose.connect(config.database_uri as string)
+    console.log('🎆😍 database connection successfully')
     app.listen(config.port, () => {
-      console.log(`listening port on ${config.port}`);
-    });
+      console.log(`listening port on ${config.port}`)
+    })
   } catch (error) {
-    console.log("😒 database connection error", error);
+    console.log('😒 database connection error', error)
   }
 }
-main();
+main()
