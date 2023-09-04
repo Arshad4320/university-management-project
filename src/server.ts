36a6374afdb3da555720760291a2fae840702920
Arshad Ullah <mdarshadullah835@gmail.com>
2023-09-04 15:35:11 +0600

    project setup done

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+async function main() {
+  try {
+    await mongoose.connect(config.database_uri as string);
+    console.log("🎆😍 database connection successfully");
+    app.listen(config.port, () => {
+      console.log(`listening port on ${config.port}`);
+    });
+  } catch (error) {
+    console.log("😒 database connection error", error);
+  }
+}
+main();
